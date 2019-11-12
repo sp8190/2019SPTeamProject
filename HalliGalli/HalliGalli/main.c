9bc346379b34e50d4247e5a8ae423bcec76a5cb3
xemic <kan5016@naver.com>
2019-11-12 16:00:53 +0900

    친절한 계원씨

---
+++ HalliGalli/HalliGalli/main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+/* 필드에 놓여있는 카드배열들인 field 들을 얻은 사용자의 덱인 deck에 추가함  */
+void TakeCardsInField(int *deck, int *field) {
+
+}
+
+/* int[4] 형태인 field에 같은 과일종류가 5개가 있는지 확인 */
+bool IsFiveFruits(int *field) {
+
+}
+
+/* 인자로 들어온 deck의 카드의 개수 반환 */
+int DeckCount(int *deck) {
+
+}
+
+/* 인자로 들어온 deck의 최상위에 push 함 */
+int Push(int *deck) {
+
+}
+
+/* 인자로 들어온 deck의 최상위값을 pop 함 */
+int Pop(int *deck) {
+
+}
+
+/* index에 해당한 카드의 과일개수 리턴 */
+int GetFruitCnt(int index) {
+
+}
+
+/* index에 해당한 카드의 과일종류 리턴 */
+int GetFruitType(int index) {
+
+}
+
+/* 사용자의 이름 수정 */
+void ModifyName() {
+
+}
+
+/* 게임 방법 설명 출력(txt 파일 읽어옴) */
+void GameDescription() {
+
+}
+
+/* halli galli 게임 시작하는함수 */
+void GameStart() {
+
+}
+
+/*  메인함수 ^^ */
+void main(void) {
+
+}
