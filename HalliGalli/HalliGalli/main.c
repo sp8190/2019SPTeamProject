24921270ca049e37db3224222114b61bfafdadca
JeonJaewon <wksk04515@naver.com>
2019-11-30 19:49:05 +0900

    gamestart 함수 초안

--- HalliGalli/HalliGalli/main.c
+++ HalliGalli/HalliGalli/main.c
@@ -1,8 +1,85 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
 #include <stdbool.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #define DECK_MAX_CNT 56
+#define PLAYER_MAX_CNT 4
 
+
+//일단 걍 전역으로 두겠음 
+int playerDeck[PLAYER_MAX_CNT][DECK_MAX_CNT]; //플레이어 4명의 덱
+
+
+/* 인자로 들어온 deck의 카드의 개수 반환 */
+int DeckCount(int *deck) {
+	int deckCount = 0;
+	int decksize = sizeof(deck) / sizeof(deck[0]); // deck 배열의 크기
+	int i;
+
+	for (i = 0; i < decksize; i++) {
+		if (deck[i] == 0) {
+			deckCount = i;
+			break;
+		}
+	}
+	return deckCount;
+}
+
+/* index에 해당한 카드의 과일개수 리턴 */
+int GetFruitCnt(int index) {
+
+	//1개짜리 5개, 2개짜리 3개, 3개짜리 3개, 4개짜리 2개, 5개짜리 1개 존재
+	//0 ~ 55 의 index 값을 인자로 받음
+	// %14 해서..
+	// 0,1,2,3,4 = 1개
+	// 5,6,7	 = 2개
+	// 8,9,10	 = 3개
+	// 11, 12	 = 4개
+	// 13		 = 5개
+
+	switch (index % 14) {
+	case 0:
+	case 1:
+	case 2:
+	case 3:
+	case 4:
+		return 1;
+	case 5:
+	case 6:
+	case 7:
+		return 2;
+	case 8:
+	case 9:
+	case 10:
+		return 3;
+	case 11:
+	case 12:
+		return 4;
+	case 13:
+		return 5;
+	default:
+		printf("GetFruitCnt error : (index mod 14) value is upper than 13");
+		return -1;
+	}
+}
+
+/* index에 해당한 카드의 과일종류 리턴 */
+int GetFruitType(int index) {
+
+	// 14개의 카드가 4개의 종류로 구분됨
+	// 0 ~ 55 의 index 값을 인자로 받음
+	// 0 ~ 13 : 과일1
+	// 14 ~ 27 : 과일2
+	// 28 ~ 41 : 과일3
+	// 42 ~ 55 : 과일4
+	// 반환값 : 0, 1, 2, 3
+
+	return (int)(index / 14);
+}
 /* 필드에 놓여있는 카드배열들인 field 들을 얻은 사용자의 덱인 deck에 추가함  */
 void TakeCardsInField(int *deck, int *field) {
 	int i;
@@ -49,20 +126,7 @@ bool IsFiveFruits(int *field) {
 	return false;
 }
 
-/* 인자로 들어온 deck의 카드의 개수 반환 */
-int DeckCount(int *deck) {
-	int deckCount = 0;
-	int decksize = sizeof(deck) / sizeof(deck[0]); // deck 배열의 크기
-	int i;
 
-	for (i = 0; i < decksize; i++) {
-		if (deck[i] == 0) {
-			deckCount = i;
-			break;
-		}
-	}
-	return deckCount;
-}
 
 /* 인자로 들어온 deck의 최상위에 push 함 */
 int Push(int *deck, int cardNum) { //보고서에는 cardNum같은 변수 없음..
@@ -83,56 +147,60 @@ int Pop(int *deck) {
 	return ret;
 }
 
-/* index에 해당한 카드의 과일개수 리턴 */
-int GetFruitCnt(int index) {
 
-	//1개짜리 5개, 2개짜리 3개, 3개짜리 3개, 4개짜리 2개, 5개짜리 1개 존재
-	//0 ~ 55 의 index 값을 인자로 받음
-	// %14 해서..
-	// 0,1,2,3,4 = 1개
-	// 5,6,7	 = 2개
-	// 8,9,10	 = 3개
-	// 11, 12	 = 4개
-	// 13		 = 5개
+/* 게임 시작 시 덱을 섞어서 플레이어들에게 나누어주는 함수 */
+void Shuffle()
+{
+    srand((unsigned)time(NULL));
+    int deck[DECK_MAX_CNT];
+    for(int i = 0; i < DECK_MAX_CNT; i++) // 덱을 만들어줌 
+        deck[i] =  i;
 
-	switch (index % 14) {
-	case 0:
-	case 1:
-	case 2:
-	case 3:
-	case 4:
-		return 1;
-	case 5:
-	case 6:
-	case 7:
-		return 2;
-	case 8:
-	case 9:
-	case 10:
-		return 3;
-	case 11:
-	case 12:
-		return 4;
-	case 13:
-		return 5;
-	default:
-		printf("GetFruitCnt error : index % 14 value is upper than 13");
-		return -1;
-	}
+    for(int i = 0; i < DECK_MAX_CNT; i++){ // 덱을 섞어줌
+        int j = rand() % DECK_MAX_CNT;
+        int tmp = 0;
+        tmp = deck[i];
+        deck[i] = deck[j];
+        deck[j] = tmp;
+    }
+
+    for(int i = 0; i < PLAYER_MAX_CNT; i++){ // 플레이어들에게 나눠줌
+        for(int j = 0; j < DECK_MAX_CNT/PLAYER_MAX_CNT; j++){
+            playerDeck[i][j] = deck[DECK_MAX_CNT/PLAYER_MAX_CNT * i + j];
+        }
+    }
 }
 
-/* index에 해당한 카드의 과일종류 리턴 */
-int GetFruitType(int index) {
+// void gotoxy(int x,int y) // 필요하면 주석 풀고 쓰면 됨
+// {
+//     printf("%c[%d;%df",0x1B,y,x);
+// }
 
-	// 14개의 카드가 4개의 종류로 구분됨
-	// 0 ~ 55 의 index 값을 인자로 받음
-	// 0 ~ 13 : 과일1
-	// 14 ~ 27 : 과일2
-	// 28 ~ 41 : 과일3
-	// 42 ~ 55 : 과일4
-	// 반환값 : 0, 1, 2, 3
 
-	return (int)(index / 14);
+void DrawPlayerCard(int playerNum)
+{
+    printf("플레이어 %d\n",playerNum+1);
+    int fruitCnt = GetFruitCnt(playerDeck[playerNum][0]); // 덱의 가장 앞에 있는 카드 출력
+    char* fruitType;
+    switch(GetFruitType(playerDeck[playerNum][0])){ 
+        case 0:
+            fruitType = "♤";
+            break;
+        case 1:
+            fruitType = "♡";
+            break;
+        case 2:
+            fruitType = "♧";
+            break;
+        case 3:
+            fruitType = "◇";
+            break;
+    }
+    printf("과일\n");
+    for(int i = 0; i < fruitCnt; i++){
+        printf("%s",fruitType);
+    }
+    printf("\n\n\n");
 }
 
 /* 사용자의 이름 수정 */
@@ -147,9 +215,46 @@ void GameDescription() {
 
 /* halli galli 게임 시작하는함수 */
 void GameStart() {
+    pid_t childPid = fork();
+    int* childStat;
+    if(childPid == -1) { // fork error
+        perror("failed to fork");
+    }
+    else if(childPid == 0){ // 자식 코드. 게임이 실행되는 프로세스
+        //게임 화면 출력하고
+        printf("--------------------------------------------------------------------------------------------------------------\n");
+        printf("\n");
+        printf("\n");
+        printf("\n");
 
+        Shuffle(); // 패 섞어서 4명에게 나누어줌
+
+        /* 패가 어떻게 섞였는지 확인하고 싶을 때 이 for문 주석 풀기 
+        for(int i = 0; i < 4; i++){ 
+            printf("플레이어 %d의 패 ",i);
+            for(int j=0; j<DECK_MAX_CNT/PLAYER_MAX_CNT; j++)
+                printf("%d  ", playerDeck[i][j]);
+            printf("\n");
+        } */
+
+        for(int i = 0; i < PLAYER_MAX_CNT; i++) // 각 플레이어의 카드패 공개
+            DrawPlayerCard(i);
+
+        printf("\n");
+        printf("\n");
+        printf("\n");
+        printf("--------------------------------------------------------------------------------------------------------------\n");
+
+        //쓰레드 두개 돌려서 게임이 진행되는 부분과 입력을 받는 부분으로 나눈다
+
+    }
+    else{ // 부모 코드
+        wait(childStat); // 부모 프로세스는 자식이 종료될 때 까지 기다림
+        printf("부모의 종료");
+    }
 }
 
 /*  메인함수 ^^ */
-void main(void) {
+int main(void) {
+    GameStart();
 }
